d5c8084307fe27eb76f8d1e87b38e63f25340c35
MarshmallowMushroom <yitong1988@gmail.com>
2014-07-04 00:25:38 -0700

    Update Mini-project#2 Guess the number.py

--- Mini-project#2 Guess the number.py
+++ Mini-project#2 Guess the number.py
@@ -1,7 +1,6 @@
 # "Guess the number" mini-project
 # input will come from buttons and an input field
 # all output for the game will be printed in the console
-#http://www.codeskulptor.org/#user29_k66skKmsZC4QZZ8.py
 
 import random
 import simplegui
